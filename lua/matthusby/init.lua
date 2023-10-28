7e840bc0af66712837af2aa32c90af7e551afd58
Matt Husby <matthusby@gmail.com>
2023-10-28 13:15:15 -0400

    init

---
+++ lua/matthusby/init.lua
@@ -0,0 +1,3 @@
+require("matthusby.remap")
+require("matthusby.packer")
+require("matthusby.set")
---
+++ lua/matthusby/remap.lua
@@ -0,0 +1,23 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- Move highlighted blocks
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- J will keep cursor at the same place when combining lines
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- Keep search terms centered
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- I do not want to be in Q land
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<leader>tt", vim.cmd.NvimTreeToggle)
+vim.keymap.set("n", "<leader>tf", vim.cmd.NvimTreeFindFile)
+
+vim.keymap.set("n", "bn", vim.cmd.bnext)
+vim.keymap.set("n", "bp", vim.cmd.bprev)
+
